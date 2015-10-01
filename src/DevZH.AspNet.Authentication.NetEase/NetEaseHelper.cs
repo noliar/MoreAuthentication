a01200a5861fb269ddf3748680e9104823e3761f
杜晓宇 <noliar@qq.com>
2015-10-01 23:39:32 +0800

    Add missing comments

--- src/DevZH.AspNet.Authentication.NetEase/NetEaseHelper.cs
+++ src/DevZH.AspNet.Authentication.NetEase/NetEaseHelper.cs
@@ -3,10 +3,20 @@ using Newtonsoft.Json.Linq;
 
 namespace DevZH.AspNet.Authentication.NetEase
 {
+    /// <summary>
+    /// Contains static methods that allow to extract user's information from a <see cref="JObject"/>
+    /// instance retrieved from NetEase after a successful authentication process.
+    /// </summary>
     internal static class NetEaseHelper
     {
+        /// <summary>
+        ///  获取用户 ID
+        /// </summary>
         internal static string GetId([NotNull] JObject user) => user.Value<string>("userId");
 
+        /// <summary>
+        ///  获取用户 名称
+        /// </summary>
         internal static string GetName([NotNull] JObject user) => user.Value<string>("username");
     }
 }
