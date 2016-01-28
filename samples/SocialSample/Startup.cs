c00d9d3416c3407e0598df8cc6d220a261752777
杜晓宇 <noliar@qq.com>
2016-01-28 12:20:09 +0800

    Reacting to new Hosting API

--- samples/SocialSample/Startup.cs
+++ samples/SocialSample/Startup.cs
@@ -6,6 +6,7 @@ using DevZH.AspNet.Authentication.Sina;
 using DevZH.AspNet.Authentication.Taobao;
 using Microsoft.AspNet.Authentication.Cookies;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Http.Authentication;
 using Microsoft.Extensions.Configuration;
@@ -201,6 +202,16 @@ namespace SocialSample
                 await context.Response.WriteAsync("<a href=\"/logout\">Logout</a>");
                 await context.Response.WriteAsync("</body></html>");
             });
+        }
+
+        public static void Main(string[] args)
+        {
+            var application = new WebApplicationBuilder()
+                .UseConfiguration(WebApplicationConfiguration.GetDefault(args))
+                .UseStartup<Startup>()
+                .Build();
+
+            application.Run();
         }
     }
 }
